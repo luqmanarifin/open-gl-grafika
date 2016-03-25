880fcb521040b536a8a81f8021d735fce5e48711
feryandi <feryandi.n@gmail.com>
2016-03-25 14:34:47 +0700

    Opening a Blank Window

--- grafika1/triangle.cpp
+++ grafika1/triangle.cpp
@@ -1,12 +1,21 @@
 // Using GLEW Library for OpenGL
 #include <GL/freeglut.h>
 
+void renderScene() {
+
+	// Clering the buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glutSwapBuffers();
+
+}
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     
     // Window Properties, Self-explanatory
     glutInitWindowPosition(100, 100);
-    glutInitWindowSize(1024, 768);
+    glutInitWindowSize(800, 600);
 
     // Window Display Mode
     // GLUT_DOUBLE = Doubel Buffer (Smooth Animation)
@@ -16,6 +25,12 @@ int main(int argc, char** argv) {
 
     // Create Window
     glutCreateWindow("Grafika 01");
+
+    // Call function to render
+    glutDisplayFunc(renderScene);
+
+    // Keep the window open and ready to receive input
+    glutMainLoop();
     
     return 0;
 }
