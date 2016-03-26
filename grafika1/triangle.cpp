55d24099c5b3e76bfbc9b4f8d30ffae99a060d82
feryandi <feryandi.n@gmail.com>
2016-03-26 13:40:03 +0700

    Nguli Warna

--- grafika1/triangle.cpp
+++ grafika1/triangle.cpp
@@ -13,8 +13,71 @@ const char* fragment_shader =
 "#version 150\n"
 "out vec4 frag_colour;"
 "void main () {"
-"  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+"  frag_colour = vec4 (0.5, 0.5, 0.5, 1.0);"
 "}";
+
+const char* fragment_shader_r =
+"#version 150\n"
+"out vec4 frag_colour;"
+"void main () {"
+"  frag_colour = vec4 (1.0, 0.0, 0.0, 1.0);"
+"}";
+
+const char* fragment_shader_g =
+"#version 150\n"
+"out vec4 frag_colour;"
+"void main () {"
+"  frag_colour = vec4 (0.0, 1.0, 0.0, 1.0);"
+"}";
+
+const char* fragment_shader_b =
+"#version 150\n"
+"out vec4 frag_colour;"
+"void main () {"
+"  frag_colour = vec4 (0.0, 0.0, 1.0, 1.0);"
+"}";
+
+void printMachineSpecification() {
+	const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
+	const GLubyte* version = glGetString (GL_VERSION); // version as a string
+	printf ("Renderer: %s\n", renderer);
+	printf ("OpenGL version supported %s\n", version);
+}
+
+void createShader(GLuint *shader_programme, const char* frsh, const char* vrsh) {
+	GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+	glShaderSource (vs, 1, &vrsh, NULL);
+	glCompileShader (vs);
+
+	GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+	glShaderSource (fs, 1, &frsh, NULL);
+	glCompileShader (fs);
+
+	*shader_programme = glCreateProgram ();
+	glAttachShader (*shader_programme, fs);
+	glAttachShader (*shader_programme, vs);
+	glLinkProgram (*shader_programme);
+}
+
+void createTriangle(GLuint *vao, float* points) {
+	GLuint vbo = 0;
+	glGenBuffers (1, &vbo);
+	glBindBuffer (GL_ARRAY_BUFFER, vbo);
+	glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (float), points, GL_STATIC_DRAW);
+
+	*vao = 0;
+	glGenVertexArrays (1, vao);
+	glBindVertexArray (*vao);
+	glEnableVertexAttribArray (0);
+	glBindBuffer (GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+}
+
+void drawTriangle(GLuint vao, GLuint shader_programme) {
+	glUseProgram (shader_programme);
+	glBindVertexArray (vao);
+	glDrawArrays (GL_TRIANGLES, 0, 3);	
+}
 
 int main(int argc, char** argv) {
 	// start GL context and O/S window using the GLFW helper library
@@ -33,10 +96,7 @@ int main(int argc, char** argv) {
 
 	glewInit ();
 
-	const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
-	const GLubyte* version = glGetString (GL_VERSION); // version as a string
-	printf ("Renderer: %s\n", renderer);
-	printf ("OpenGL version supported %s\n", version);
+	printMachineSpecification();
 
 	// tell GL to only draw onto a pixel if the shape is closer to the viewer
 	glEnable (GL_DEPTH_TEST); // enable depth-testing
@@ -45,45 +105,51 @@ int main(int argc, char** argv) {
 
 	float points[] = {
 	   0.0f,  0.5f,  0.0f,
+	   0.25f, 0.0f,  0.0f,
+	   -0.25f, 0.0f,  0.0f
+	};
+
+	GLuint vao;
+	createTriangle(&vao, points);
+
+	float points2[] = {
+	   0.25f,  0.0f,  0.0f,
 	   0.5f, -0.5f,  0.0f,
+	  0.0f, -0.5f,  0.0f
+	};
+
+	GLuint vao2;
+	createTriangle(&vao2, points2);
+
+	float points3[] = {
+	   -0.25f,  0.0f,  0.0f,
+	   0.0f, -0.5f,  0.0f,
 	  -0.5f, -0.5f,  0.0f
 	};
 
-	GLuint vbo = 0;
-	glGenBuffers (1, &vbo);
-	glBindBuffer (GL_ARRAY_BUFFER, vbo);
-	glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (float), points, GL_STATIC_DRAW);
+	GLuint vao3;
+	createTriangle(&vao3, points3);
 
-	GLuint vao = 0;
-	glGenVertexArrays (1, &vao);
-	glBindVertexArray (vao);
-	glEnableVertexAttribArray (0);
-	glBindBuffer (GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	GLuint shader_programme;
+	createShader(&shader_programme, fragment_shader, vertex_shader);
 
-	GLuint vs = glCreateShader (GL_VERTEX_SHADER);
-	glShaderSource (vs, 1, &vertex_shader, NULL);
-	glCompileShader (vs);
-	GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
-	glShaderSource (fs, 1, &fragment_shader, NULL);
-	glCompileShader (fs);
-
-	GLuint shader_programme = glCreateProgram ();
-	glAttachShader (shader_programme, fs);
-	glAttachShader (shader_programme, vs);
-	glLinkProgram (shader_programme);
+	GLuint shader_programme_r;
+	createShader(&shader_programme_r, fragment_shader_r, vertex_shader);
+	GLuint shader_programme_g;
+	createShader(&shader_programme_g, fragment_shader_g, vertex_shader);
+	GLuint shader_programme_b;
+	createShader(&shader_programme_b, fragment_shader_b, vertex_shader);
 
 	while (!glfwWindowShouldClose (window)) {
-	  // wipe the drawing surface clear
-	  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	  glUseProgram (shader_programme);
-	  glBindVertexArray (vao);
-	  // draw points 0-3 from the currently bound VAO with current in-use shader
-	  glDrawArrays (GL_TRIANGLES, 0, 3);
-	  // update other events like input handling 
-	  glfwPollEvents ();
-	  // put the stuff we've been drawing onto the display
-	  glfwSwapBuffers (window);
+		// wipe the drawing surface clear
+		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		drawTriangle(vao, shader_programme_r);
+		drawTriangle(vao2, shader_programme_g);
+		drawTriangle(vao3, shader_programme_b);
+		// update other events like input handling 
+		glfwPollEvents ();
+		// put the stuff we've been drawing onto the display
+		glfwSwapBuffers (window);
 	}
 
 	glfwTerminate();
